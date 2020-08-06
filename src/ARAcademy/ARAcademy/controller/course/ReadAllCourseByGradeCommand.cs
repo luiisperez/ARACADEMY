5874db9f2a3c252e8d6f859f9e1d675a5fe92d75
luiisperez <luis.alejandro120895@gmail.com>
2020-08-06 23:46:25 +0200

    Comandos

---
+++ src/ARAcademy/ARAcademy/controller/course/ReadAllCourseByGradeCommand.cs
@@ -0,0 +1,28 @@
+﻿using ARAcademy.common.entities;
+using ARAcademy.model.course;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ARAcademy.controller.course
+{
+    public class ReadAllCourseByGradeCommand : Command
+    {
+        Grade grade;
+        private List<Course> courses;
+
+        public ReadAllCourseByGradeCommand(Grade grade)
+        {
+            this.grade = grade;
+        }
+
+        public List<Course> Courses { get => courses; set => courses = value; }
+
+        public override void Execute()
+        {
+            DAOCourse daoCourse = new DAOCourse();
+            courses = daoCourse.ReadAllCoursesByGrade(grade);
+        }
+    }
+}
