7925deecce71f6f42b5508e2867070c78ecde5e9
luiisperez <luis.alejandro120895@gmail.com>
2020-08-07 09:52:03 +0200

    Estatus añadido

--- src/ARAcademy/ARAcademy/common/Entity.cs
+++ src/ARAcademy/ARAcademy/common/Entity.cs
@@ -8,7 +8,10 @@ namespace ARAcademy.common
     public abstract class Entity
     {
         private int code;
+        private char status;
 
         public int Code { get => code; set => code = value; }
+
+        public char Status { get => status; set => status = value; }
     }
 }
