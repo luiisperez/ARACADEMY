a73611a34cebba7be26a22209c75722be72346d0
luiisperez <luis.alejandro120895@gmail.com>
2020-08-09 21:11:05 +0200

    Cambios

---
+++ src/ARAcademy/ARAcademy/site/test.aspx.cs
@@ -0,0 +1,21 @@
+﻿using ARAcademy.common.entities;
+using ARAcademy.controller.section;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ARAcademy.site
+{
+    public partial class test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ReadAllSectionByCourseCommand cmd = new ReadAllSectionByCourseCommand(new Course("1"));
+            cmd.Execute();
+            List<Section> sections = cmd.Sections;
+        }
+    }
+}
