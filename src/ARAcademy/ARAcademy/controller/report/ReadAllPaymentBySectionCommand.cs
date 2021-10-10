08b1a5fe0647e0f2e4202abb61183ed06737a209
luiisperez <luis.alejandro120895@gmail.com>
2021-10-10 03:32:20 +0200

    Reportes back

---
+++ src/ARAcademy/ARAcademy/controller/report/ReadAllPaymentBySectionCommand.cs
@@ -0,0 +1,33 @@
+﻿using ARAcademy.common.entities;
+using ARAcademy.model.payment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ARAcademy.controller.report
+{
+    public class ReadAllPaymentBySectionCommand : Command
+    {
+        Section section;
+        private List<AraPayment> payments;
+
+        public ReadAllPaymentBySectionCommand(Section section)
+        {
+            this.section = section;
+        }
+
+        public List<AraPayment> Payments { get => payments; set => payments = value; }
+
+        public override void Execute()
+        {
+            DAOPayment daoPayment = new DAOPayment();
+            payments = daoPayment.ReadAllPaymentsBySection(section);
+        }
+
+        public int CountTotalPayments()
+        {
+            return payments.Count();
+        }
+    }
+}
